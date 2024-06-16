c643a6e01a4dd0612f729179a6d395f570816a24
SalimNhiri <81355011+SalimNhiri@users.noreply.github.com>
2024-06-17 01:12:21 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 
 from routers import films, users, auth
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
         
 app.include_router(users.router) 
 app.include_router(films.router)
 app.include_router(auth.router)
 
+app.get("/")
+def root():
+    return {"message": "Hello World pushing out to ubuntu"}
