62c9271496fdb984227f3410f74ad5f4161687df
SalimNhiri <81355011+SalimNhiri@users.noreply.github.com>
2024-06-27 10:54:09 +0200

    Update profil.py
    adding profil model

--- models/profil.py
+++ models/profil.py
@@ -1,1 +1,14 @@
+from typing import Optional, Any,List
+from datetime import datetime
 
+from pydantic import BaseModel, EmailStr
+
+
+class Profil(BaseModel):
+    id: str
+    user_id: str
+    position: str
+    companie : EmailStr
+    #tech : List[str]
+    linkedin : str
+    GitHub : str
